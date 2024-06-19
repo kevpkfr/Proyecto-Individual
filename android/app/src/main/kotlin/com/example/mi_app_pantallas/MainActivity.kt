0d3b00f9a925999a5ef3390e1b77319427add213
kpinsag <kpinsag@gmail.com>
2024-06-18 23:24:59 -0500

    first commit

--- android/app/src/main/kotlin/com/example/mi_app_pantallas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mi_app_pantallas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mi_app_pantallas/main.dart';
+import 'package:tienda_online/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
